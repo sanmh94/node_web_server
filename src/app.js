fd0547802ed41aa88013071b791b20ff2cefb3e2
sanmh94 <167009201+sanmh94@users.noreply.github.com>
2024-09-29 21:31:30 +0200

    realizamos un servidor web donde servimos una web de tipo red social como la del pajarito

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const { envs } = require("./config/env");
+const {startServer} = require('./server/server')
+
+const main = () => {
+  startServer({
+    port:envs.PORT,
+    public_path:envs.PUBLIC_PATH
+  })
+};
+
+//funcion agnostica autoconvocada
+//agnostica porque no tiene nombre
+//autoconvocada porque la ejecutamos con los parentesis
+(async () => {
+  main();
+})();
