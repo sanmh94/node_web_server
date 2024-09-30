69edfd10cbc2bc2d8f56c60e1ec8834dc67fdc5c
sanmh94 <167009201+sanmh94@users.noreply.github.com>
2024-09-30 10:43:29 +0200

    Realizamos modificaciones para que funcione con importaciones y exportaciones de tipo ES6

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const path = require('path')
+import express from 'express'
+import  path  from 'path'
 
-const startServer = (options) => {
+export const startServer = (options) => {
     const {port, public_path = 'public'} = options
     const app = express()
     
     //para poder usar middleware se usa express --> configuran nuestra applicacion
-    app.use(express.static(public_path)) //contenido estatico que ponemos disponible
+    app.use(express.static(public_path)); //contenido estatico que ponemos disponible
     //para que si nos hacen una peticion, es un servidor// ahora mismo devuelvo la web
     app.get("*", (req, res)=>{
         const indexPath = path.join(__dirname + `../../../${public_path}/index.html`)
@@ -18,8 +18,4 @@ const startServer = (options) => {
         console.log(`Escuando en el puerto ${port}`)
     })
 
-}
-
-module.exports = {
-    startServer
 }
